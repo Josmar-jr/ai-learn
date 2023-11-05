345cdbce1823864b4943b8e0ddc54432a5303fe2
Josmar Soares Trigueiro Junior <josmar.junior@cubos.io>
2023-11-05 12:10:25 -0300

    fix: adjust script build

--- src/app/(app)/lessons/page.tsx
+++ src/app/(app)/lessons/page.tsx
@@ -3,7 +3,10 @@ import Image from 'next/image'
 export default function Lessons() {
   return (
     <div>
-      <Image src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" />
+      <Image
+        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
+        alt="Dev Icon Test"
+      />
     </div>
   )
 }
